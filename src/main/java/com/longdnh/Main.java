def8b3fe59cc4778e18f69e68c30d31a1290f9ba
longdnh <longnh.uit@gmail.com>
2026-06-17 22:56:49 +0700

    first problem

--- src/main/java/com/longdnh/Main.java
+++ src/main/java/com/longdnh/Main.java
@@ -1,7 +1,10 @@
 package com.longdnh;
 
+import com.longdnh.dsa.longest_common_prefix.Solution;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chào mừng đến với Java Console!");
+        Solution solution = new Solution();
+        System.out.println(solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
     }
 }
---
+++ src/main/java/com/longdnh/dsa/longest_common_prefix/Solution.java
@@ -0,0 +1,8 @@
+package com.longdnh.dsa.longest_common_prefix;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder longestCommonPrefix = new StringBuilder();
+        
+    }
+}
