c95cae6d617f5f5f338065da0029b4d482f5dae0
longdnh <longnh.uit@gmail.com>
2026-06-18 08:06:37 +0700

    longest common prefix solved

--- src/main/java/com/longdnh/dsa/longest_common_prefix/Solution.java
+++ src/main/java/com/longdnh/dsa/longest_common_prefix/Solution.java
@@ -3,6 +3,33 @@ package com.longdnh.dsa.longest_common_prefix;
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder longestCommonPrefix = new StringBuilder();
-        
+        String prefix = strs[0];
+        int prefLen = prefix.length();
+        for (int i=1;i<strs.length;i++){
+            String s = strs[i];
+            while(prefLen > s.length() || !prefix.equals(s.substring(0, prefLen))){
+                prefLen--;
+                if (prefLen == 0) return "";
+                prefix = prefix.substring(0, prefLen);
+            }
+        }
+        return prefix;
     }
 }
+// my solution
+
+//class Solution {
+//    public String longestCommonPrefix(String[] strs) {
+//        StringBuilder longestCommonPrefix = new StringBuilder();
+//        for (int i = 0; i < strs[0].length(); i++) {
+//            char c = strs[0].charAt(i);
+//            for (int j = 1; j < strs.length; j++) {
+//                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+//                    return longestCommonPrefix.toString();
+//                }
+//            }
+//            longestCommonPrefix.append(c);
+//        }
+//        return longestCommonPrefix.toString();
+//    }
+//}
