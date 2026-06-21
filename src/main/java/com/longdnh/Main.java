aa9c20e2ed69e47be9b5f1e36c468d0fe24d367f
longdnh <longnh.uit@gmail.com>
2026-06-21 23:06:08 +0700

    feat(string-array): Add longest substring solution scaffold
    - Add initial solution class for longest substring without repeats
    - Update main demo to run length of last word example

--- src/main/java/com/longdnh/Main.java
+++ src/main/java/com/longdnh/Main.java
@@ -1,11 +1,11 @@
 package com.longdnh;
 
 
-import com.longdnh.dsa.string_array.indexOfTheFirstOccurrece.Solution;
+import com.longdnh.dsa.string_array.length_of_last_word.Solution;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.strStr("leetcode", "code"));
+        System.out.println(solution.lengthOfLastWord("Hello World"));
     }
 }
---
+++ src/main/java/com/longdnh/dsa/string_array/LongestSubStringWithoutRepeatingChar/Solution.java
@@ -0,0 +1,10 @@
+package com.longdnh.dsa.string_array.LongestSubStringWithoutRepeatingChar;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Character[] arr = new Character[s.length()];
+        arr[0] = s.charAt(0);
+        int length = 1;
+        return length;
+    }
+}
