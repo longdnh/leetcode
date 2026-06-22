d694354a6fa07d408e626e035cbe9d90f586ca7e
longdnh <longnh.uit@gmail.com>
2026-06-22 20:45:44 +0700

    feat(string-array): Add binary string addition solution
    - Add solution for summing two binary strings
    - Update main entry point to run the add binary example

--- src/main/java/com/longdnh/Main.java
+++ src/main/java/com/longdnh/Main.java
@@ -1,11 +1,11 @@
 package com.longdnh;
 
 
-import com.longdnh.dsa.string_array.length_of_last_word.Solution;
+import com.longdnh.dsa.string_array.add_binary.Solution;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.lengthOfLastWord("Hello World"));
+        System.out.println(solution.addBinary("101011", "1011"));
     }
 }
