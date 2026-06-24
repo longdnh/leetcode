9dcaac7ec51485087b54f62d7a007823e9bb23d5
longdnh <longnh.uit@gmail.com>
2026-06-24 21:02:40 +0700

    feat(string-array): Add remove duplicates solution

---
+++ src/main/java/com/longdnh/dsa/string_array/search_insert_position/Solution.java
@@ -0,0 +1,17 @@
+package com.longdnh.dsa.string_array.search_insert_position;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
