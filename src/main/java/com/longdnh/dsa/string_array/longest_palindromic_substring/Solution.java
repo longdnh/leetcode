1e97c3942095a5d7e02ab3af26ff061275600a90
longdnh <longnh.uit@gmail.com>
2026-06-24 21:29:27 +0700

    chore(string-array): Add longest palindrome solution scaffold

---
+++ src/main/java/com/longdnh/dsa/string_array/longest_palindromic_substring/Solution.java
@@ -0,0 +1,14 @@
+package com.longdnh.dsa.string_array.longest_palindromic_substring;
+
+import java.util.HashSet;
+
+public class Solution {
+//    public String longestPalindrome(String s) {
+//        HashSet<Character> set = new HashSet<>();
+//        for(int i=0;i<s.length();i++){
+//            set.add(s.charAt(i));
+//            if (i > 0 && s.charAt(i) == s.charAt(i-1)) continue;
+//
+//        }
+//    }
+}
